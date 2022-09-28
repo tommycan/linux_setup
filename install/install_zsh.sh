95ba11a0e858b5719b6df3ed6392df19804e2b89
tommy andersson <tommy@haleytek.com>
2022-09-28 14:26:11 +0200

    Add .zshrc, .zshrc_aliases and install_zsh.sh

---
+++ install/install_zsh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# install packages
+sudo apt install zsh
+sudo apt-get install fzf
+
+# install oh-my-zsh
+# https://github.com/ohmyzsh/ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install oh-my-zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
