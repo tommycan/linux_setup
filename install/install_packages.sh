417676e363bb6abb1e5e1c5d6cd1cd22f48291a1
tommy andersson <tommy@haleytek.com>
2022-10-07 08:56:51 +0200

    Extend install_packages.sh

--- install/install_packages.sh
+++ install/install_packages.sh
@@ -2,10 +2,22 @@
 
 sudo apt update
 sudo apt install \
+	meld \
+	openssh-server \
+	repo \
+	python3-pip \
 	tmux \
-	vim \
 	tree \
-	meld
+	vim \
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# vs code
+# https://linuxhint.com/install-visual-studio-code-ubuntu22-04/
 
+# docker
+# https://linuxhint.com/install-docker-ubuntu-22-04/
+# https://cloudcone.com/docs/article/how-to-install-docker-on-ubuntu-22-04-20-04/
+
+pip3 install docker-compose
+
+# tmux plugins
+# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
