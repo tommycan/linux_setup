19f80d44cc195a6de04213bb2470bb8d8ad87029
Tommy Andersson <tommy@haleytek.com>
2023-05-18 08:48:12 +0200

    Add zsh-auto-notify

--- dotfiles/.zshrc
+++ dotfiles/.zshrc
@@ -71,6 +71,7 @@ ZSH_THEME="robbyrussell"
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
 plugins=(
+    auto-notify
     # bgnotify
     # fzf
     # fzf-tab
--- install/install_zsh.sh
+++ install/install_zsh.sh
@@ -15,3 +15,8 @@ chsh -s $(which zsh)
 git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# https://github.com/MichaelAquilina/zsh-auto-notify/issues/46
+git clone https://github.com/MichaelAquilina/zsh-auto-notify.git $ZSH_CUSTOM/plugins/auto-notify
+# cd $ZSH_CUSTOM/plugins/auto-notify git checkout 1f64cb654473d7208f46534bc3df47ac919d4a72
+
